9637c609b9b48ccdfcdf47a3e9cedf1f65c558e3
Janiralph <janiralph@gmail.com>
2023-04-04 06:27:12 -0700

    node to a list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,11 +21,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		*head = new_node;
 		return (new_node);
-
 	}
 	else
 	{
-		tmp - *head;
+		tmp = *head;
 		while (tmp->next != NULL)
 		{
 			tmp = tmp->next;
