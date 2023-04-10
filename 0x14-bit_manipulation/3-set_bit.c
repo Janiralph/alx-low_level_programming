50a3d411274955bc22740dc3128e048388bf0f82
Janiralph <janiralph@gmail.com>
2023-04-09 23:50:04 -0700

    sets a bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,14 +9,14 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int j = 1UL << index;
+	unsigned long int k = 1UL << index;
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	*n |= j
+	*n |= k;
 
 	return (1);
 }
