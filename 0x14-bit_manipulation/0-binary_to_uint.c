9f9c6b183ed04d1f044bf7aa9ef030f59b7702d6
Janiralph <janiralph@gmail.com>
2023-04-09 23:36:36 -0700

    binry to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,11 +3,12 @@
 /**
  * binary_to_uint - convert a binary number to an unsigned int
  * @b: char string
- * Return: converted decimal number or 0
+ * Return: a binary number
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0
+	unsigned int num = 0;
 
 	if (b == NULL)
 	{
