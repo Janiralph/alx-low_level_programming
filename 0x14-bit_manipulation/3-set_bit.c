1a57a628f39c5575248bd9002b456aa06e687af9
Janiralph <janiralph@gmail.com>
2023-04-09 23:48:33 -0700

    set a bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,12 +6,17 @@
  * @index: Index that sets value
  * Return: status error or alternaative a -1.
  */
-int set_bit(unsigned long int *n, unsigned int index);
-{
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
 
-	*n ^= (1 << index);
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int j = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n |= j
 
 	return (1);
 }
